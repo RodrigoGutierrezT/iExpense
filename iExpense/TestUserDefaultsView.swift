5bb8a7c5e43949f46a78d3df04bf199c554e5ba3
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-14 22:04:54 -0400

    feat: Add Codable and JSONEncoder example

--- iExpense/TestUserDefaultsView.swift
+++ iExpense/TestUserDefaultsView.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
+
 struct TestUserDefaultsView: View {
     @State private var tapCount = UserDefaults.standard.integer(forKey: "tap")
     @AppStorage("count") private var count = 0
     
+    @State private var user = User(firstName: "Mario", lastName: "Bros")
+    
     var body: some View {
+        
+        Spacer()
+        
         Text("Tap count: \(tapCount)")
         Button("Tap me!") {
             tapCount += 1
@@ -22,6 +32,18 @@ struct TestUserDefaultsView: View {
         Button("App storage tap!") {
             count += 1
         }
+        
+        Spacer()
+        
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
+        
+        Spacer()
     }
 }
 
